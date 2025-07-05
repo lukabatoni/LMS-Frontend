2c3ec40e2440846baa266b8ce2404314b823489d
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-05 20:06:57 +0400

    refactor: reorganize navigation and routes for better readability

--- lms-fronend/src/App.js
+++ lms-fronend/src/App.js
@@ -3,22 +3,25 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import StudentForm from './components/StudentForm';
 import StudentList from './components/StudentList';
 import CourseList from './components/CourseList';
+import LessonList from './components/LessonList'; 
 
 function App() {
   return (
     <Router>
-      <nav style={{ padding: '20px', backgroundColor: '#eee' }}>
-        <Link to="/students" style={{ marginRight: '15px' }}>View Students</Link>
-        <Link to="/students/create" style={{ marginRight: '15px' }}>Create Student</Link>
-        <Link to="/courses">View Courses</Link>
-      </nav>
+<nav style={{ padding: '20px', backgroundColor: '#eee' }}>
+  <Link to="/students" style={{ marginRight: '15px' }}>View Students</Link>
+  <Link to="/students/create" style={{ marginRight: '15px' }}>Create Student</Link>
+  <Link to="/courses" style={{ marginRight: '15px' }}>View Courses</Link>
+  <Link to="/lessons">View Lessons</Link> 
+</nav>
 
-      <Routes>
-        <Route path="/students" element={<StudentList />} />
-        <Route path="/students/create" element={<StudentForm />} />
-        <Route path="/courses" element={<CourseList />} />
-        <Route path="*" element={<p style={{ padding: '20px' }}>404 Page Not Found</p>} />
-      </Routes>
+<Routes>
+  <Route path="/students" element={<StudentList />} />
+  <Route path="/students/create" element={<StudentForm />} />
+  <Route path="/courses" element={<CourseList />} />
+  <Route path="/lessons" element={<LessonList />} />
+  <Route path="*" element={<p style={{ padding: '20px' }}>404 Page Not Found</p>} />
+</Routes>
     </Router>
   );
 }
