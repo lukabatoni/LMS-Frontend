3c4e2f55795d792992c239d6788ad577b858bd01
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-07 16:33:04 +0400

    feat: add CourseForm route to navigation

--- lms-fronend/src/App.js
+++ lms-fronend/src/App.js
@@ -4,25 +4,28 @@ import StudentForm from './components/StudentForm';
 import StudentList from './components/StudentList';
 import CourseList from './components/CourseList';
 import LessonList from './components/LessonList'; 
+import CourseForm from './components/CourseForm';
 
 function App() {
   return (
-    <Router>
-<nav style={{ padding: '20px', backgroundColor: '#eee' }}>
-  <Link to="/students" style={{ marginRight: '15px' }}>View Students</Link>
-  <Link to="/students/create" style={{ marginRight: '15px' }}>Create Student</Link>
-  <Link to="/courses" style={{ marginRight: '15px' }}>View Courses</Link>
-  <Link to="/lessons">View Lessons</Link> 
-</nav>
+<Router>
+  <nav style={{ padding: '20px', backgroundColor: '#eee' }}>
+    <Link to="/students" style={{ marginRight: '15px' }}>View Students</Link>
+    <Link to="/students/create" style={{ marginRight: '15px' }}>Create Student</Link>
+    <Link to="/courses" style={{ marginRight: '15px' }}>View Courses</Link>
+    <Link to="/courses/create" style={{ marginRight: '15px' }}>Create Course</Link>
+    <Link to="/lessons">View Lessons</Link>
+  </nav>
 
-<Routes>
-  <Route path="/students" element={<StudentList />} />
-  <Route path="/students/create" element={<StudentForm />} />
-  <Route path="/courses" element={<CourseList />} />
-  <Route path="/lessons" element={<LessonList />} />
-  <Route path="*" element={<p style={{ padding: '20px' }}>404 Page Not Found</p>} />
-</Routes>
-    </Router>
+  <Routes>
+    <Route path="/students" element={<StudentList />} />
+    <Route path="/students/create" element={<StudentForm />} />
+    <Route path="/courses" element={<CourseList />} />
+    <Route path="/courses/create" element={<CourseForm />} /> {/* ✅ New route */}
+    <Route path="/lessons" element={<LessonList />} />
+    <Route path="*" element={<p style={{ padding: '20px' }}>404 Page Not Found</p>} />
+  </Routes>
+</Router>
   );
 }
 
